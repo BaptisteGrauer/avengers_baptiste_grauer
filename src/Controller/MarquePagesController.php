bf07b70e3823c285ba5a0b6306f81739af577100
Baptiste <baptiste.grosr@gmail.com>
2024-02-19 08:49:47 +1100

    avdj 3

--- src/Controller/MarquePagesController.php
+++ src/Controller/MarquePagesController.php
@@ -10,9 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/marque-pages', requirements: ["_locale" => "en|es|fr"], name: "marquepage_")]
 class MarquePagesController extends AbstractController
 {
-    #[Route('/', name: 'marque_pages')]
+    #[Route('/', name: 'index')]
     public function index(EntityManagerInterface $entityManager): Response
     {
         $marque_pages = $entityManager->getRepository(MarquePage::class)->findAll();
@@ -21,8 +22,8 @@ class MarquePagesController extends AbstractController
         ]);
     }
 
-    #[Route('/ajouter')]
-    public function ajoutMarquePage(EntityManagerInterface $entityManager){
+    #[Route('/ajouter', name: "ajouter")]
+    public function ajoutMarquePage(EntityManagerInterface $entityManager){ // pas bon exemple
         $marque_page = new MarquePage();
         $marque_page->setUrl("https://site.nc/");
         $marque_page->setDateCreation(new \DateTime());
@@ -30,5 +31,16 @@ class MarquePagesController extends AbstractController
         $entityManager->persist($marque_page);
         $entityManager->flush();
         return new Response("Marque page ajouté avec succès (id :".$marque_page->getId().")");
+    }
+
+    #[Route('/details/{id}', name:'details')]
+    public function detail(EntityManagerInterface $entityManager, int $id): Response {
+        $marquePage = $entityManager->getRepository(MarquePage::class)->find($id);
+        if (!$marquePage) {
+            throw $this->createNotFoundException("Le marque-page demandé n'existe pas");
+        }
+        return $this->render('marque_pages/detail.html.twig', [
+            'marquePage' => $marquePage,
+        ]);
     }
 }
